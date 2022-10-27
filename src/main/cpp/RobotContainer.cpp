5256ac01ea67fbe15eb359331ab1cdc319dce4e6
Ian Walke <iandwbusiness690@gmail.com>
2022-10-26 19:57:22 -0500

    Drive Integration WIP

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -3,8 +3,14 @@
 // the WPILib BSD license file in the root directory of this project.
 
 #include "RobotContainer.h"
+#include <frc2/command/RunCommand.h>
 
-RobotContainer::RobotContainer() {
+
+constexpr double thresh = 0.01;
+constexpr double boostMult = 1.5;
+
+
+RobotContainer::RobotContainer() : m_controller{0} {
   // Initialize all of your commands and subsystems here
 
   //set our default command (default commands basically run on start on the program)
@@ -18,6 +24,18 @@ RobotContainer::RobotContainer() {
         // This is where all of the systems come together. Call DriveSubsystem::ArcadeDrive
         // from here, or functions from any other subsystem. This function will run periodically 
         // for each subsystem
+        double leftY = Deadband(m_controller.GetRawAxis(static_cast<int>(frc::XboxController::Axis::kLeftY)), thresh); //left stick, forward/backward
+        double rightX = Deadband(m_controller.GetRawAxis(static_cast<int>(frc::XboxController::Axis::kRightX)), thresh); //right stick, left/right
+        bool leftBumper = m_controller.GetRawButton((static_cast<int>(frc::XboxController::Button::kLeftBumper))); //left bumper, boost
+        
+        if (leftBumper) {
+          leftY *= boostMult;
+          rightX *= boostMult;
+        }
+
+        m_drive.ArcadeDrive(leftY, rightX);
+        
+        
 
   }, {&m_drive}));
 
@@ -31,6 +49,11 @@ void RobotContainer::ConfigureButtonBindings() {
 
 double RobotContainer::Deadband(double input, double threshold){
   // TODO implement deadband function
+  if (abs(input) < threshold) {
+    return 0;
+  } else {
+    return input;
+  }
 }
 
 frc2::Command* RobotContainer::GetAutonomousCommand() {
